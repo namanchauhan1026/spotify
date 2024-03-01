eddc5b22873fe70a570a4d62658b50fa63fa334f
namanchauhan1026 <109587400+namanchauhan1026@users.noreply.github.com>
2024-03-01 23:01:51 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -159,7 +159,7 @@ async function displayAlbums() {
         let cardContainer = document.querySelector(".cardContainer");
         let array = Array.from(anchors);
 
-        for (let index = 2; index < array.length; index++) {
+        for (let index = 0; index < array.length; index++) {
             const e = array[index];
             if (e.href.includes("/songs") && !e.href.includes(".htaccess")) {
                 let folder = e.href.split("/").slice(-2)[0];
